6ef75189ab61de20d16d591fbf4770397f391ab9
Macha Celeste <shetah1@yahoo.com>
2025-05-01 11:32:09 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BetterZDSPlugin;
+
+[BepInPlugin("com.machaceleste.BetterZDSPlugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<bool> disable;
+    public static ConfigEntry<int> daysValid;
+    public static ConfigEntry<int> modMonths;
+        
+    private void Awake()
+    {
+        disable = Config.Bind("Main", "Disable zero days", false, "Disable the entire ZDS rumor system.");
+        daysValid = Config.Bind("Main", "Days rumors available", 15, new ConfigDescription("Number of days the rumors are available for.", new AcceptableValueRange<int>(1, 25)));
+        modMonths = Config.Bind("Main", "Months modulo", 2, new ConfigDescription("Month % n == 0 Determines how often rumors are available.", new AcceptableValueRange<int>(1, 12)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.BetterZDSPlugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/ZeroDaySystemPatch.cs
@@ -0,0 +1,23 @@
+﻿using BetterZDSPlugin;
+using HarmonyLib;
+using System;
+
+[HarmonyPatch]
+public class ZeroDaySystemPatch
+{
+    [HarmonyPatch(typeof(ZeroDaySystem), "IsRumorTime")]
+    class IsRumorTimePatch
+    {
+        static bool Prefix(ZeroDaySystem __instance, ref bool __result)
+        {
+            if (!Plugin.disable.Value)
+            {
+                DateTime dateTime = ClockServer.Singleton.GetDateTime();
+                __result = dateTime.Month % Plugin.modMonths.Value == 0 && dateTime.Day < Plugin.daysValid.Value;
+            }
+            else 
+                __result = false;
+            return false;
+        }
+    }
+}
